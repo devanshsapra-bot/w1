33c27d5eb268b9f28e6f5e5eaf39926b04ce6dc6
23104035 <23104035@pdc.jiit>
2025-07-30 11:23:45 +0530

    2nd

---
+++ week2/q1.py
@@ -0,0 +1,4 @@
+fruits = ["apple","mango","dragon","aam","Orange"]
+#print(fruits)
+print(f"First fruit : {fruits[0]} and Last fruit : {fruits[-1]}")
+print(fruits[1:4])
---
+++ week2/q2.py
@@ -0,0 +1,12 @@
+dname = {
+    "Devansh" : 20,
+    "Abmishraji" : 20,
+    "Coder_for_fun" : 19,
+    "randosm" : 19,
+}
+print(dname)
+
+print(f"Age of Devansh is:{dname["Devansh"]}")
+
+dname["CP"]=21
+print(dname)
